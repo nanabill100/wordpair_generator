8a330c888a1c278ab045e915efdea8079083d0fc
nanabill100 <nanabill100@gmail.com>
2022-04-22 16:33:45 +0000

    WordPair Generator App

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme:
+            ThemeData(primaryColor: const Color.fromARGB(255, 209, 130, 209)),
+        home: const RandomWords());
+  }
+}
